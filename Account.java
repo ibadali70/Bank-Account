d3800113865db50e79b340b8eeb2f80c331349c3
Ibad Ali <ibadali769@gmail.com>
2025-04-28 03:25:23 +0500

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,45 @@
+//Ibad Ali
+//2412177
+package Practice;
+
+class Account {
+    int amt;
+    private int balance = 5000;
+
+    void setBalance(int amt) {
+        balance = amt;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
+
+class ATM {
+    Account user;
+
+    ATM() {
+        user = new Account();
+    }
+
+    void balanceCheck() {
+        System.out.println("Current Balance: " + user.getBalance());
+    }
+
+    void withdraw(int amount) {
+        int bal = user.getBalance();
+        if (bal >= 500 && amount <= bal) {
+            if (amount % 500 == 0) {
+                bal = bal - amount;
+                user.setBalance(bal);
+                System.out.println("Withdrawal Successful. New Balance: " + bal);
+            } 
+            else {
+                System.out.println("Invalid Amount (must be a multiple of 500)");
+            }
+        } 
+        else {
+            System.out.println("Low Balance");
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class Main {
+    public static void main(String[] args) {
+        ATM HBL = new ATM();
+        
+        HBL.balanceCheck();
+        HBL.withdraw(1000);
+    }
+}
